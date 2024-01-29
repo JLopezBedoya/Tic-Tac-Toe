a9c842bd32ad32c9726b007d90f871f3f9d7b0e7
JLopezBedoya <jlopezbedoya1011@gmail.com>
2024-01-29 18:39:12 -0500

    gh-pages fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://JLopezBedoya.github.io/tic-tac-toe'
+  base: "https://jlopezbedoya.github.io/Tic-Tac-Toe"
 })
