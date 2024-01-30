61b72545a5b3fee77f697222b1a47612b4035ed6
JLopezBedoya <jlopezbedoya1011@gmail.com>
2024-01-30 17:11:02 -0500

    input

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -2,11 +2,6 @@ import { useState } from "react";
 import { FaO, FaX, FaArrowRotateRight } from "react-icons/fa6";
 
 function Game({data, setData}){
-    const [state, setState] = useState([
-        0,1,0,
-        2,2,0,
-        0,0,1
-    ])
     const comeBack = ()=>{
         setData({
             show: true, 
@@ -14,38 +9,60 @@ function Game({data, setData}){
             game: data.game,
         })
     }
+    const [state, setState] = useState([
+        0,0,0,
+        0,0,0,
+        0,0,0
+    ])
+    const [ties, setTies] = [{
+        you: 16,
+        ties: 32,
+        cpu: 12
+    }]
+    const [player, setPlayer] = useState(1)
     return(
         <section className="game" style={{left: ((data.show) ? "200%":"0")}}>
             <article className="game-header">
                 <section className="game-header-icons"><span className="cross"><FaX /></span>  <span className="circle"><FaO /></span></section>
-                <section className="game-header-turn"> {(true) ? <span className="cross"><FaX /></span>:<span className="circle"><FaO /></span>} </section>
+                <section className="game-header-turn"> {(player==1) ? <span className="cross"><FaX /></span>:<span className="circle"><FaO /></span>} </section>
                 <section className="game-header-return" onClick={comeBack}><FaArrowRotateRight /></section>
             </article>
-            <Board game={state} setGame={setState} />
+            <Board player={player} setPlayer={setPlayer} game={state} setGame={setState} />
             <article className="game-footer">
                 <section className="game-footer-you">
                     <h4>You</h4>
-                    <h2>13</h2>
+                    <h2>{ties.you}</h2>
                 </section>
                 <section className="game-footer-ties">
                     <h4>Ties</h4>
-                    <h2>35</h2>
+                    <h2>{ties.ties}</h2>
                 </section>
                 <section className="game-footer-cpu">
                     <h4>CPU</h4>
-                    <h2>10</h2>
+                    <h2>{ties.cpu}</h2>
                 </section>
             </article>
         </section>
     )
 }
 
-function Board({game, setGame}){
-    const players = [<></>, <FaO/>, <FaX/>]
+function Board({game, setGame, player, setPlayer}){
+    const players = [<FaArrowRotateRight />, <FaX/>, <FaO/>]
+    console.log(player);
+    const playerhandle = (id, posc)=>{
+        if(posc == 0){
+            setGame(prev => {
+                const newGame = [...prev];
+                newGame[id] = player;
+                return newGame;
+            });
+            setPlayer((player==1) ? 2:1)
+        }
+    }
     return(
         <article className="game-board">
             {game.map((e,i)=>(
-                <section className={(e==1) ? "circle":"cross"} key={"position"+i}> {players[e]} </section>
+                <section onClick={()=>playerhandle(i, e)} className={(e==1) ? "cross":(e==2)?"circle":"space"} key={"position"+i}> {players[e]} </section>
             ))}
         </article>
     )
